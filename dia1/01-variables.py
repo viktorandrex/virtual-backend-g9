19c0657d531230b6d4b69fe2b2518f850b6e71f5
viktorandrex <victorconsultores@outlook.com>
2021-08-16 21:22:08 -0500

    feat: added a newgregue el archivo variable.py

---
+++ dia1/01-variables.py
@@ -0,0 +1,29 @@
+#Esto es un comentario
+
+#variables de texto
+#------------------
+nombre = "eduardo"
+apellido = 'de rivero'
+#nombre = 20
+#Las comillas pueden ocacionar problemas como en el siguiente caso
+#direccion = 'calle o'higgins 125'
+direccion = "calle o'higgins 125"
+# texto = "hola amigos:
+# buenas noches, les saluda su profe
+# atentamente, 
+# eduardo
+texto = """hola amigos:
+buenas noches, les saluda su profe
+atentamente, 
+eduardo"""
+texto2 = '''hola amigos:
+ya casi viene el break'''
+#para guardar cn salto de pagina se debe usar triple doble comilla o la triple simple comilla
+
+#variables numericas
+#------------------
+año = 2021
+estatura = 1.92
+
+print(type(nombre))
+print(type(año))
